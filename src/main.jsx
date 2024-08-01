5fd5a571eb270dbaafbb90c6db7b0ee7dfd3dbe0
29yabuki <skybluenako@gmail.com>
2024-08-01 17:15:08 +0800

    added ReactGA

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ReactGA from 'react-ga4';
+import App from './App.jsx';
+import './index.css';
+
+ReactGA.initialize('G-6P9RBM7XVN');
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
